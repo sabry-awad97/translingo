17b666591d4717bde24f1316c7f084ca0c3b990c
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-27 13:19:25 +0200

    reading from a file path or from standard input

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@ pub mod translation_cache;
 pub mod translator;
 pub mod utils;
 
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, Read},
+};
 
 use structopt::StructOpt;
 
@@ -14,8 +18,8 @@ use crate::{config::TranslateOptions, translator::translate};
 #[derive(StructOpt, Debug)]
 #[structopt(name = "translator", about = "A command line translator tool")]
 pub struct Cli {
-    #[structopt(help = "The input text to translate")]
-    pub input: String,
+    #[structopt(help = "The input source (file path or \"-\" for stdio)")]
+    pub input_source: Option<String>,
 
     #[structopt(short = "f", long = "from", help = "The source language code")]
     pub from: Option<String>,
@@ -24,10 +28,39 @@ pub struct Cli {
     pub to: Option<String>,
 }
 
+enum InputSource {
+    File(String),
+    Stdio,
+}
+
+impl Cli {
+    fn get_input_source(&self) -> Result<InputSource, std::io::Error> {
+        match self.input_source.as_deref() {
+            None | Some("-") => Ok(InputSource::Stdio),
+            Some(file_path) => Ok(InputSource::File(file_path.to_owned())),
+        }
+    }
+}
+
 pub async fn run() -> Result<(), Box<dyn Error>> {
     let args = Cli::from_args();
+    let input_source = args.get_input_source()?;
+    let input = match input_source {
+        InputSource::File(file_path) => {
+            let mut file = File::open(file_path)?;
+            let mut input = String::new();
+            file.read_to_string(&mut input)?;
+            input
+        }
+        InputSource::Stdio => {
+            let mut input = String::new();
+            io::stdin().read_to_string(&mut input)?;
+            input
+        }
+    };
+
     let options = TranslateOptions::new(args.from, args.to);
-    let response = translate(&args.input, options).await?;
+    let response = translate(&input, options).await?;
     println!("{}", serde_json::to_string(&response)?);
 
     Ok(())
